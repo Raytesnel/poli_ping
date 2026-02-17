cce8783fcf7f7c3110e7c096e25b05e07ad80d8c
Raytesnel <raymondhouwing@gmail.om>
2026-02-17 22:44:57 +0100

    init frontend landing page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,11 +1,58 @@
-mod guide_component;
-
 use dioxus::prelude::*;
+use reqwest::get;
+
 fn main() {
     dioxus::launch(App);
 }
+static CSS: Asset = asset!("/assets/main.css");
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+        document::Stylesheet { href: CSS }
+        Title {}
+        DogView {}
+    }
 }
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    rsx! {
+        div { id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default() }
+        }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
+        }
+    }
+}
+
+
+
+
+

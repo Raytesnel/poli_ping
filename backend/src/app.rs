f6b499278e99935379499819cc6897edc43241a0
Raytesnel <raymondhouwing@gmail.om>
2026-02-18 00:33:30 +0100

    add first communication between frontend and backend

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,10 +1,18 @@
 use axum::Router;
-
+use tower_http::cors::{Any, CorsLayer};
 use crate::routes::moties;
 
 pub async fn run() {
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any) // allow all origins for dev
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+
     let app = Router::new()
-        .merge(moties::routes());
+        .merge(moties::routes())
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
--- shared/src/api.rs
+++ shared/src/api.rs
@@ -1,1 +1,2 @@
 pub const GET_MOTIES: &str = "/get_moties";
+pub const GET_FIRST_MOTIE: &str = "/get_first_motie";

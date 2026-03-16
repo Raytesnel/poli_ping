819fd7ede7095da1ec5aef9c13d541588b3c0d76
raytesnel <raymondhouwing@gmail.com>
2026-03-16 16:09:14 +0100

    add voting test

--- backend/src/repository/vote.rs
+++ backend/src/repository/vote.rs
@@ -7,7 +7,7 @@ pub async fn insert_user_vote(
     vote: &str,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-            r#"
+        r#"
         INSERT INTO user_votes (user_id, motie_id, vote)
         VALUES (?, ?, ?)
         ON CONFLICT(user_id, motie_id)
@@ -17,8 +17,58 @@ pub async fn insert_user_vote(
         motie_id,
         vote
     )
-        .execute(pool)
-        .await?;
+    .execute(pool)
+    .await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use uuid::Uuid;
+    use crate::models::db_models::UserVotes;
+
+    async fn seed_motie(pool: &SqlitePool) -> i64 {
+        let random_id = Uuid::new_v4().to_string();
+        sqlx::query!(
+            r#"
+        INSERT INTO moties (external_id,title,description,result,timestamp)
+        VALUES (?,'title','desc','Aangenomen','2026')
+        RETURNING id
+        "#,
+            random_id
+        )
+        .fetch_one(pool)
+        .await
+        .unwrap()
+        .id
+    }
+    #[sqlx::test]
+    async fn save_user_votes_to_db(pool: SqlitePool) {
+        let motie_id = seed_motie(&pool).await;
+        let user_id = "Ash";
+        let vote = "tegen";
+        // Act
+        let result = insert_user_vote(&pool, &user_id, motie_id as i32, vote).await;
+
+        assert!(result.is_ok());
+
+        let vote_result = sqlx::query_as!(
+            UserVotes,
+        r#"
+        SELECT user_id,motie_id, vote
+        FROM user_votes
+        WHERE motie_id = ?
+        "#,
+        motie_id
+    )
+            .fetch_all(&pool)
+            .await.unwrap();
+        assert_eq!(vote_result.len(), 1);
+        assert_eq!(vote_result.first().unwrap().user_id, user_id);
+        assert_eq!(vote_result.first().unwrap().vote, vote);
+
+    }
+
 }
